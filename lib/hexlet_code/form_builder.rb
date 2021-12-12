f58e52852210ef87511ee9bc6c97947a5c6f1f3b
Kirill Ilyin <cuurjol@gmail.com>
2021-12-13 02:23:40 +0300

    Fix hexlet CI and add label/submit features

--- lib/hexlet_code/form_builder.rb
+++ lib/hexlet_code/form_builder.rb
@@ -11,13 +11,35 @@ module HexletCode
     end
 
     def input(attribute, **options)
+      as_key = options.delete(:as)
       value = model.public_send(attribute)
-      as = options.delete(:as)
-      tag_name = as.nil? ? 'input' : 'textarea'
-      attributes = options.merge(name: attribute.to_s)
-      attributes.update(as.nil? ? { type: 'text', value: value } : { cols: 20, rows: 40 })
-      tag = Tag.build(tag_name, attributes) { value unless as.nil? }
-      form_inner_tags.push(tag)
+      attributes = options.merge(name: attribute)
+      tag = as_key.nil? ? build_input_tag(value, attributes) : build_textarea_tag(value, attributes)
+      label_tag = build_label_tag(attribute, for: attribute)
+      form_inner_tags.push(label_tag, tag)
+    end
+
+    def submit(button_name = 'Save')
+      attributes = { name: 'commit', type: 'submit', value: button_name }
+      form_inner_tags.push(Tag.build('input', **attributes))
+    end
+
+    private
+
+    def build_textarea_tag(value, attributes)
+      attributes.update(cols: 20) if attributes[:cols].nil?
+      attributes.update(rows: 40) if attributes[:rows].nil?
+      Tag.build('textarea', **attributes) { value }
+    end
+
+    def build_input_tag(value, attributes)
+      attributes.update(value: value) unless value.nil?
+      attributes.update(type: 'text')
+      Tag.build('input', **attributes)
+    end
+
+    def build_label_tag(value, attributes)
+      Tag.build('label', **attributes) { value.to_s.capitalize }
     end
   end
 end
