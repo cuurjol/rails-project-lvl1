b74ef72bc4f796b5d8f994f240e89e0ed24cb677
Kirill Ilyin <cuurjol@gmail.com>
2021-12-13 00:41:41 +0300

    Fix hexlet CI

--- lib/hexlet_code/form_builder.rb
+++ lib/hexlet_code/form_builder.rb
@@ -10,10 +10,11 @@ module HexletCode
       @form_inner_tags = []
     end
 
-    def input(attribute, as: nil)
+    def input(attribute, **options)
       value = model.public_send(attribute)
+      as = options.delete(:as)
       tag_name = as.nil? ? 'input' : 'textarea'
-      attributes = { name: attribute.to_s }
+      attributes = options.merge(name: attribute.to_s)
       attributes.update(as.nil? ? { type: 'text', value: value } : { cols: 20, rows: 40 })
       tag = Tag.build(tag_name, attributes) { value unless as.nil? }
       form_inner_tags.push(tag)
